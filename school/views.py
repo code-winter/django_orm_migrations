d2f1c3cdbece48403593e5656311a463e1cfd0af
Dmitry Popov <ant1h4t3rz@gmail.com>
2022-02-23 11:29:21 +0900

    finished hw

---
+++ school/views.py
@@ -0,0 +1,25 @@
+from django.views.generic import ListView
+from django.shortcuts import render
+import json
+from .models import Student
+
+if not Student.teacher:
+    with open('school.json', 'r', encoding='utf8') as file:
+        data = json.load(file)
+        for student in data:
+            if student['model'] == 'school.student':
+                for entry in Student.objects.filter(name=student['fields']['name']):
+                    entry.teacher.add(student['fields']['teacher'])
+
+
+def students_list(request):
+    template = 'school/students_list.html'
+    ordering = 'group'
+    student_obj = Student.objects.order_by(ordering).prefetch_related('teacher')
+    context = {
+        'object_list': student_obj,
+    }
+
+    # используйте этот параметр для упорядочивания результатов
+    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
+    return render(request, template, context)
